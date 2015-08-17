eabb4c40d4e006716b8bd74a338031518589d01f
Aleksander Madland Stapnes <aleksander_m_stapnes@hotmail.com>
2015-08-16 22:58:58 -0300

    Partial fix for magit color scheme.

--- customizations/editing.el
+++ customizations/editing.el
@@ -80,3 +80,13 @@
   (save-some-buffers t))
 
 (add-hook 'focus-out-hook 'save-all)
+
+;; Try to fix magit colors
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")
+     ;; TODO: Figure out how to change the magit highlight.
+     ;(set-face-foreground 'magit-item-highlight "green3")
+     (unless window-system
+       (set-face-background 'magit-item-highlight "black"))))
